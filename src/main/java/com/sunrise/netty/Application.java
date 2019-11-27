e4a911604eaa429142fda6db02fb6f9221caab78
lzhaoyang <575563079@qq.com>
2019-11-27 23:58:40 +0800

    init the repo

---
+++ src/main/java/com/sunrise/netty/Application.java
@@ -0,0 +1,13 @@
+package com.sunrise.netty;
+
+/**
+ * @description:
+ * @version: 1.00
+ * @author: lzhaoyang
+ * @date: 2019/11/27 11:56 PM
+ */
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Hello netty!!!");
+    }
+}
