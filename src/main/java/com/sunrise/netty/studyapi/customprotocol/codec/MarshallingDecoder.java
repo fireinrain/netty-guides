8450008c35273333a08e49102f32d7a25221e88d
lzhaoyang <575563079@qq.com>
2019-12-18 23:07:32 +0800

    update

---
+++ src/main/java/com/sunrise/netty/studyapi/customprotocol/codec/MarshallingDecoder.java
@@ -0,0 +1,39 @@
+package com.sunrise.netty.studyapi.customprotocol.codec;
+
+import io.netty.buffer.ByteBuf;
+import org.jboss.marshalling.Unmarshaller;
+
+import java.io.IOException;
+
+/**
+ * @description:
+ * @version: 1.00
+ * @author: lzhaoyang
+ * @date: 2019/12/18 10:48 PM
+ */
+public class MarshallingDecoder {
+
+    private final Unmarshaller unmarshaller;
+
+    public MarshallingDecoder() throws IOException {
+        this.unmarshaller = MarshallingCodecFactory.buildUnMarshalling();
+    }
+
+    protected Object decode(ByteBuf in) throws Exception{
+        //获取大小
+        int objectSize = in.readInt();
+        //截取
+        ByteBuf byteBuf = in.slice(in.readerIndex(), objectSize);
+        ChannelBufferByteInput byteInput = new ChannelBufferByteInput(byteBuf);
+        try {
+            unmarshaller.start(byteInput);
+            Object object = unmarshaller.readObject();
+            unmarshaller.finish();
+            //标记为读完了
+            in.readerIndex(in.readerIndex()+objectSize);
+            return object;
+        }finally {
+            unmarshaller.close();
+        }
+    }
+}
--- src/main/java/com/sunrise/netty/studyapi/customprotocol/codec/NettyMessageDecoder.java
+++ src/main/java/com/sunrise/netty/studyapi/customprotocol/codec/NettyMessageDecoder.java
@@ -4,15 +4,19 @@ import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
 
+import java.io.IOException;
+
 /**
  * @description: netty 消息解码器
  * @date: 2019/12/14
  * @author: lzhaoyang
  */
 public class NettyMessageDecoder extends LengthFieldBasedFrameDecoder {
+    private MarshallingDecoder marshallingDecoder;
 
-    public NettyMessageDecoder(int maxFrameLength, int lengthFieldOffset, int lengthFieldLength) {
+    public NettyMessageDecoder(int maxFrameLength, int lengthFieldOffset, int lengthFieldLength) throws IOException {
         super(maxFrameLength, lengthFieldOffset, lengthFieldLength);
+        this.marshallingDecoder = new MarshallingDecoder();
     }
 
     @Override
